02428cb228665023ad7cc2e5c70c5f8ab171aec2
G'aybullayev - IT Blog <123540083+gaybullayevblog@users.noreply.github.com>
2024-02-06 10:37:37 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'hello_world',
-    'pages',
+    # 'pages',
+    'static_files',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILE_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('hello_world.urls')),
-    path('', include('pages.urls'))
+    # path('', include('pages.urls')),
+    path('', include('static_files.urls')),
 ]
